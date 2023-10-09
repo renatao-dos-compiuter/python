7f5f7ef72c3e26ccbd7da11c0110be70ac6a36d1
renatao2023santos <pablonholoko@gmail.com>
2023-10-09 07:53:39 -0300

    exercícios complementares

---
+++ Logica_de_programacao/aula16/ex1.py
@@ -0,0 +1,6 @@
+colunas=int(input("informe a quantidade de colunas: "))
+linhas=int(input("informe a quantidade de linhas: "))
+
+for i in range (colunas):
+    for i2 in range (linhas):
+        print("*", end= ' ')
---
+++ Logica_de_programacao/aula16/ex2.py
@@ -0,0 +1,6 @@
+lado= int (input("Informe o valor do lado: "))
+
+for i in range (lado):
+    for c in range (lado):
+        print("*", end= ' ')
+    print()
